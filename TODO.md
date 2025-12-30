3d71ec564a097c1a14c014a70c7fb62cfdfc0ad1
Riccardo <rtrocca@gmail.com>
2025-12-30 10:05:06 +0100

    move to Poetry

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# TODOs
+
+- it would be useful to allow to overrun parameters like model and temperature to see how a prompt behaves
--- run_basic_test.sh
+++ run_basic_test.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Activate virtual environment if it exists
-if [ -d ".venv" ]; then
-    source .venv/bin/activate
-fi
-
 # Run the prompt
-python run.py basic-test \
+poetry run promptrunner basic-test \
     '{"assistant_name": "Rufus", "user_question": "Explain me in detail how text embeddings work and how a transformer model can be used to calculate them"}' \
     --prompts-file prompts/prompts.yaml \
     --models-file prompts/models.yaml \
